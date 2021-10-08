c080baba045d67db433042d413253db058dcb073
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-10-08 21:33:38 +0000

    Set up Express Api

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
-import { AppController } from '../controllers/AppController';
-
 const express = require('express');
-const app = require('express');
+const AppController = require('../controllers/AppController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-app.get('/', (req, res) => {
-  res.send('key: value');
-});
-
-module.exports = app;
+module.exports = router;
